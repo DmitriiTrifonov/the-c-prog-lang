040ba33c3e6ff82c767735074ca50f268ac8cd0f
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2019-10-30 17:19:12 +0300

    Update and rename 01_chapter_1/05_1_exercise_1_6.c to chapter-01/exercise1-6/exercise1-6.c

--- chapter-01/exercise1-6/exercise1-6.c
+++ chapter-01/exercise1-6/exercise1-6.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int c;
-    while ((c = getchar()) != EOF)
-    {
+    while ((c = getchar()) != EOF) {
         printf("%d\n", getchar() != EOF);
     }
     return 0;
