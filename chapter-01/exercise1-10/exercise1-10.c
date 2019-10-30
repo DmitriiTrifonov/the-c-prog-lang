b7d5e923006706f06acdcab528b51240db4cbc8f
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2019-10-30 17:28:05 +0300

    Update and rename 01_chapter_1/05_3_exercise_1_10.c to chapter-01/exercise1-10/exercise1-10.c

--- chapter-01/exercise1-10/exercise1-10.c
+++ chapter-01/exercise1-10/exercise1-10.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 
-
 int main()
 {
     char c;
-    int n; //n is used as trigger for typing tabs, backspace or backslashes
+    int n; // n is used as trigger for typing tabs, backspace or backslashes
     while ((c = getchar()) != EOF){
         n = 0;
-        if (c == '\t')
-        {
+        if (c == '\t') {
             putchar('\\');
             putchar('t');
             n = 1;
         }
-        if (c == 8)
-        {
+        if (c == 8) {
             putchar('\\');
             putchar('b');
             n = 1;
         }
-        if (c == '\\')
-        {
+        if (c == '\\') {
             putchar('\\');
             putchar('\\');
             n = 1;
         }
-
         if (n == 0) putchar(c);
     }
     return 0;
